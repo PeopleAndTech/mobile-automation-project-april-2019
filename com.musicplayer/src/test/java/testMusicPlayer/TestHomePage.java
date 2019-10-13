cc79e65e02bb844f88ccad6123eec504b80e1ea5
Zannatun Naim Sheum <zann@zannatuns-mbp-3.home>
2019-10-13 17:13:11 -0400

    base

---
+++ com.musicplayer/src/test/java/testMusicPlayer/TestHomePage.java
@@ -0,0 +1,22 @@
+package testMusicPlayer;
+
+import Pages.HomePage;
+import common.MobileAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestHomePage extends MobileAPI {
+    HomePage homePage;
+
+    @BeforeMethod
+    public void init() {
+        homePage = PageFactory.initElements(appiumDriver, HomePage.class);
+    }
+
+
+    @Test
+    public void uiTestCases() {
+        System.out.println("test");
+    }
+}
